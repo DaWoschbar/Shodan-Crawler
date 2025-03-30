4b4a5085a0cc14ea9e1e188151896be673521950
DaWoschbar <schabusr@proton.me>
2025-03-30 19:51:59 +0200

    Implemented simple fetch

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from dotenv import load_dotenv
 import shodan
 import os
+import requests
 
 # load the secrets from the .env file
 load_dotenv()
 api_key = os.getenv("API_KEY")
-countries = 'de' # 'at,de,ch'
+countries = 'DE' # 'at,de,ch'
 products = 'tasmota'
 
+found_ips = []
+
+dbg_count = 0
 
 try:
     api = shodan.Shodan(api_key)
+    query = f'country:{countries} {products}'
 
-    print(f'[i] Currently searching for: country:{countries} product:{products}')
+    print(f'[DBG] Currently searching for: {query}')
+    results = api.search(query, 5)
 
-    results = api.search(f'country:{countries} product:{products}', 5)
-    print(results)
+    for service in results['matches']:
+        if (dbg_count > 5):
+            break
+        else:
+            dbg_count += 1
+
+        found_ips.append(service['ip_str'])
+
+        try:
+            r = requests.get(f'http://{service['ip_str']}:{service['port']}', timeout=10)
+            print(r)
+        except requests.exceptions.Timeout:
+            print(f"Timeout reached - skipped for {service['ip_str']}:{service['port']}")
+
 
 except Exception as e:
-    print("Something unexepected happend:" + e)
+    print("Something unexepected happend!")
+    print(e)
