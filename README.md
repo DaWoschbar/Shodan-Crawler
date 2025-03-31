f6ea488d51d676307912d12e60cc8c3deaf77e35
DaWoschbar <schabusr@proton.me>
2025-03-31 11:39:38 +0200

    Implemented asynchronous functionality to query multiple targets at once

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Shodan-Crawler
 Simple Shodan Crawler Proof of Concept for my Bachelor Thesis 
+
+# ToDo's
+
+- [x] Make the requests async to guarantee better performance
+- [ ] Check for SSL issues (and ignore them)
+- [ ] Pretty output
+- [ ] Save all the data in a meaningful output
+- [ ] Make automated screenshots using something like selenium
--- main.py
+++ main.py
@@ -3,38 +3,62 @@ import shodan
 import os
 import requests
 
+import httpx
+import asyncio
+
 # load the secrets from the .env file
 load_dotenv()
 api_key = os.getenv("API_KEY")
-countries = 'DE' # 'at,de,ch'
+countries = 'DE' # 'AT,DE,CH'
 products = 'tasmota'
+debug = 0
 
-found_ips = []
+targets = []
 
-dbg_count = 0
+def queryShodanDevices():
+        # Connecting to shodan using the API-Key
+        api = shodan.Shodan(api_key)
+        # Query shodan for the given country and product
+        query = f'country:{countries} {products}'
 
-try:
-    api = shodan.Shodan(api_key)
-    query = f'country:{countries} {products}'
+        print(f'[i] Currently searching for: {query}')
+        # Limit search results to 5 pages - currently only for developing purposes
+        results = api.search(query, 5)
 
-    print(f'[DBG] Currently searching for: {query}')
-    results = api.search(query, 5)
+        # go through the matches and save the ip + port for later asynchronmotor requests
+        for service in results['matches']:
+            targets.append(f"{service['ip_str']}:{service['port']}")
 
-    for service in results['matches']:
-        if (dbg_count > 5):
-            break
-        else:
-            dbg_count += 1
+async def queryRequests():
+    try:
+        # Creating async-client to to through the found ip + port with limited connections
+        async with httpx.AsyncClient(limits=httpx.Limits(max_connections=10)) as client:
+            tasks = [fetchDevice(client, target) for target in targets]
+            results = await asyncio.gather(*tasks)
 
-        found_ips.append(service['ip_str'])
-
-        try:
-            r = requests.get(f'http://{service['ip_str']}:{service['port']}', timeout=10)
-            print(r)
-        except requests.exceptions.Timeout:
-            print(f"Timeout reached - skipped for {service['ip_str']}:{service['port']}")
+    except Exception as e:
+        if debug:
+            print(f'Something unexepected happend! \n {e}')
 
 
-except Exception as e:
-    print("Something unexepected happend!")
-    print(e)
+async def fetchDevice (client, target):
+    try:
+        # trying to fetch found target
+        url = f'http://{target}'
+        response = await client.get(url, timeout=10)
+        if response.status_code == 200:
+            print(f'Success for: {url}')
+        return response.status_code
+
+    except Exception as e:
+        # The crawl can fail for various reasons such as timeout, unavailability, read errors, etc.
+        if debug:    
+            print(f'Error fetching {url} - {e}')
+
+
+async def main():
+    debug = 0
+    queryShodanDevices()
+    await queryRequests()
+
+asyncio.run(main())
